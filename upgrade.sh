b7fb3d5651f215b993d6cc5c533efa9b5ffe89ef
Brett Rea <6890798+brettjrea@users.noreply.github.com>
2023-02-24 22:25:55 -0500

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -5,7 +5,7 @@ DEBIAN_FRONTEND=noninteractive
 apt update -y && apt dist-upgrade -y
 ###Configure available releases to LTS.
 sed -i 's/Prompt=lts/Prompt=normal/g' /etc/update-manager/release-upgrades
-###Change from Buster to Bullseye.
+###Change from Focal to Jammy.
 sed -i 's/focal/jammy/g' /etc/apt/sources.list
 apt update -y
 apt upgrade -y
