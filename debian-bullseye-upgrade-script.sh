55b83fbef010d76bde4da802e4244d6ab926829e
brettjrea <brettjrea@users.noreply.github.com>
2023-03-19 01:10:13 +0000

    Auto commit copied ubuntu-jammy-upgrade-script.sh

--- debian-bullseye-upgrade-script.sh
+++ debian-bullseye-upgrade-script.sh
@@ -5,11 +5,10 @@ export DEBIAN_FRONTEND=noninteractive
 apt update -y && apt dist-upgrade -y
 ###Configure available releases to LTS.
 sed -i 's/Prompt=lts/Prompt=normal/g' /etc/update-manager/release-upgrades
-###Change from Buster to Bullseye.
-sed -i 's/buster/bullseye/g' /etc/apt/sources.list
+###Change from Focal to Jammy.
+sed -i 's/focal/jammy/g' /etc/apt/sources.list
 apt update -y
 apt upgrade -y
 apt dist-upgrade -y
 apt autoremove -y
 apt clean -y
-
